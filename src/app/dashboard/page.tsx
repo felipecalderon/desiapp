950d647168e60ad980df9f81ceae2ce7f3a7f5c2
Felipe <felipe.calderon321@gmail.com>
2023-08-30 19:47:52 -0400

    nada

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Dashboard'
+  }
+
+const Dashboard = async () => {
+  return (
+    <div>Dash</div>
+  )
+}
+
+export default Dashboard
