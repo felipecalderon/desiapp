5b7c79fd3ec0829f480eb09504db0535420aec4f
Felipe <felipe.calderon321@gmail.com>
2024-01-11 16:51:54 -0300

    boton descargar excel

--- src/components/tablas/GenerarTabla.tsx
+++ src/components/tablas/GenerarTabla.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import TablaProductos from './ProductTable'
 import storeProduct from '@/stores/store.product'
 import { bajarExcel } from '@/utils/toExcel'
+import { RiFileExcel2Fill } from "react-icons/ri";
 
 const GenerarTabla = () => {
     const { products } = storeProduct()
@@ -11,7 +12,7 @@ const GenerarTabla = () => {
     }
     return (
         <div className="container mx-auto px-10 dark:bg-gray-800">
-            <button onClick={download} className="bg-blue-700 mt-3 px-3 py-2 text-white rounded-lg h-fit hover:bg-blue-500 transition-all">Descargar excel</button>
+            <button onClick={download} className="flex flex-row items-center bg-blue-700 mt-3 px-3 py-2 text-white rounded-lg h-fit hover:bg-blue-500 transition-all">Descargar excel <RiFileExcel2Fill className='text-2xl ml-2' /></button>
             <div className="bg-white dark:bg-gray-900 shadow-md rounded my-6 overflow-x-auto">
                 <TablaProductos products={products}/>
             </div>
