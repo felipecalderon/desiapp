4a69ded72273681b18654471b28749d9591408e0
Felipe <felipe.calderon321@gmail.com>
2024-11-08 17:30:31 -0300

    talla en lista homepage

--- src/components/SalesResumeTable.tsx
+++ src/components/SalesResumeTable.tsx
@@ -1,18 +1,28 @@
 'use client'
-import storeDataStore from "@/stores/store.dataStore";
-import storeSales from "@/stores/store.sales";
-import { getFecha } from "@/utils/fecha";
-import { formatoPrecio } from "@/utils/price";
-import { useRouter } from "next/navigation";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
-import FiltroVentas from "./FiltroVentas";
+import storeDataStore from '@/stores/store.dataStore'
+import storeSales from '@/stores/store.sales'
+import { getFecha } from '@/utils/fecha'
+import { formatoPrecio } from '@/utils/price'
+import { useRouter } from 'next/navigation'
+import {
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+} from '@nextui-org/react'
+import FiltroVentas from './FiltroVentas'
 
 const SalesResumeTable = () => {
     const { filteredSales } = storeSales()
     const { stores } = storeDataStore()
     const route = useRouter()
 
-    const redireccionVenta = (saleID: string, esOC: boolean | "" | undefined) => {
+    const redireccionVenta = (
+        saleID: string,
+        esOC: boolean | '' | undefined
+    ) => {
         if (!esOC) route.push(`/vender/${saleID}`)
         else route.push(`/comprar/detalle/${saleID}`)
     }
@@ -30,35 +40,91 @@ const SalesResumeTable = () => {
                     <TableColumn>Estado</TableColumn>
                 </TableHeader>
                 <TableBody>
-                    {ventasFiltradas.map(({ total, status, createdAt, saleID, storeID, SaleProducts, type }, index) => {
-                        const creacion = getFecha(createdAt);
-                        const store = stores && stores.find(({ storeID: ID }) => ID === storeID)
-                        const esOC = type && type === 'OC'
-                        const ventasOC = SaleProducts.reduce((acc, variacion) => {
-                            return acc += variacion.quantityOrdered ? variacion.quantityOrdered : variacion.quantitySold
-                        }, 0)
-                        const esUltimoDiaMes = index === ventasFiltradas.length - 1 || new Date(createdAt).getMonth() !== new Date(ventasFiltradas[index + 1].createdAt).getMonth();
-                        const noEsDelFinal = index !== ventasFiltradas.length - 1 && esUltimoDiaMes
-                        return (
-                            <TableRow key={saleID}
-                                onClick={() => redireccionVenta(saleID, esOC)}
-                                className={`${noEsDelFinal && 'border-b-4 border-lime-300'}
-                                ${esOC 
-                                    ? 'bg-blue-100 hover:bg-blue-300 hover:cursor-pointer' 
-                                    : 'hover:bg-gray-100 dark:hover:bg-blue-700 hover:cursor-pointer'}`}
-                            >
-                                <TableCell> {store && store.name} </TableCell>
-                                <TableCell> {creacion?.fecha} - {creacion?.hora}hrs </TableCell>
-                                <TableCell> {formatoPrecio(total)} </TableCell>
-                                <TableCell> {
-                                    ventasOC < 3
-                                        ? <p>{SaleProducts[0].quantitySold}x {SaleProducts[0].name} {ventasOC > 1 && <span className="text-green-700 font-bold">(+{ventasOC-1})</span>} </p>
-                                        : <p>{ventasOC} pares vendidos</p>
-                                } </TableCell>
-                                <TableCell> {status} </TableCell>
-                            </TableRow>
-                        );
-                    })}
+                    {ventasFiltradas.map(
+                        (
+                            {
+                                total,
+                                status,
+                                createdAt,
+                                saleID,
+                                storeID,
+                                SaleProducts,
+                                type,
+                            },
+                            index
+                        ) => {
+                            const creacion = getFecha(createdAt)
+                            const store =
+                                stores &&
+                                stores.find(({ storeID: ID }) => ID === storeID)
+                            const esOC = type && type === 'OC'
+                            const ventasOC = SaleProducts.reduce(
+                                (acc, variacion) => {
+                                    return (acc += variacion.quantityOrdered
+                                        ? variacion.quantityOrdered
+                                        : variacion.quantitySold)
+                                },
+                                0
+                            )
+                            const esUltimoDiaMes =
+                                index === ventasFiltradas.length - 1 ||
+                                new Date(createdAt).getMonth() !==
+                                    new Date(
+                                        ventasFiltradas[index + 1].createdAt
+                                    ).getMonth()
+                            const noEsDelFinal =
+                                index !== ventasFiltradas.length - 1 &&
+                                esUltimoDiaMes
+                            return (
+                                <TableRow
+                                    key={saleID}
+                                    onClick={() =>
+                                        redireccionVenta(saleID, esOC)
+                                    }
+                                    className={`${
+                                        noEsDelFinal &&
+                                        'border-b-4 border-lime-300'
+                                    }
+                                ${
+                                    esOC
+                                        ? 'bg-blue-100 hover:bg-blue-300 hover:cursor-pointer'
+                                        : 'hover:bg-gray-100 dark:hover:bg-blue-700 hover:cursor-pointer'
+                                }`}
+                                >
+                                    <TableCell>
+                                        {' '}
+                                        {store && store.name}{' '}
+                                    </TableCell>
+                                    <TableCell>
+                                        {' '}
+                                        {creacion?.fecha} - {creacion?.hora}hrs{' '}
+                                    </TableCell>
+                                    <TableCell>
+                                        {' '}
+                                        {formatoPrecio(total)}{' '}
+                                    </TableCell>
+                                    <TableCell>
+                                        {' '}
+                                        {ventasOC < 3 ? (
+                                            <p>
+                                                [T{SaleProducts[0].sizeNumber}]{' '}
+                                                {SaleProducts[0].quantitySold}x{' '}
+                                                {SaleProducts[0].name}{' '}
+                                                {ventasOC > 1 && (
+                                                    <span className="text-green-700 font-bold">
+                                                        (+{ventasOC - 1})
+                                                    </span>
+                                                )}{' '}
+                                            </p>
+                                        ) : (
+                                            <p>{ventasOC} pares vendidos</p>
+                                        )}{' '}
+                                    </TableCell>
+                                    <TableCell> {status} </TableCell>
+                                </TableRow>
+                            )
+                        }
+                    )}
                 </TableBody>
             </Table>
         </>
