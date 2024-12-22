6ddbc7d83efa50aeb251b0d9384c569feb1190f7
Felipe <felipe.calderon321@gmail.com>
2024-12-22 20:29:23 -0300

    crear productos ready

--- src/components/FiltroVentas.tsx
+++ src/components/FiltroVentas.tsx
@@ -1,73 +1,86 @@
-import { Sales } from "@/config/interfaces";
-import storeSales from "@/stores/store.sales";
-import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
-import { useEffect, useMemo, useState } from "react";
+import { Sales } from '@/config/interfaces'
+import storeSales from '@/stores/store.sales'
+import { Autocomplete, AutocompleteItem } from '@nextui-org/react'
+import { useEffect, useMemo, useState } from 'react'
 
 const FiltroVentas = () => {
-  const { sales, setFilterMonth, setFilterYear } = storeSales();
+    const { sales, setFilterMonth, setFilterYear } = storeSales()
 
-  // Preparación de datos para meses y años
-  const months = useMemo(() => ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"], []);
-  const years = useMemo(() => Array.from(new Set(sales.map(sale => new Date(sale.createdAt).getFullYear()))).sort(), [sales]);
+    // Preparación de datos para meses y años
+    const months = useMemo(
+        () => ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        []
+    )
+    const years = useMemo(() => Array.from(new Set(sales.map((sale) => new Date(sale.createdAt).getFullYear()))).sort(), [sales])
 
-  // Gestores de cambio de selección
-  const monthSelectionChange = (key: React.Key) => {
-    if(!key) setFilterMonth(null)
-    else setFilterMonth(key.toString().toLowerCase());
-  };
-  
-  const yearSelectionChange = (key: React.Key) => {
-    if(!key) setFilterYear(null)
-    else setFilterYear(key.toString());
-  };
+    // Gestores de cambio de selección
+    const monthSelectionChange = (key: React.Key | null) => {
+        if (!key) setFilterMonth(null)
+        else setFilterMonth(key.toString().toLowerCase())
+    }
 
-  const [nombreFecha, setFecha] = useState<{ mes: string | undefined, year: string | undefined }>({
-    mes: undefined,
-    year: undefined
-  })
+    const yearSelectionChange = (key: React.Key | null) => {
+        if (!key) setFilterYear(null)
+        else setFilterYear(key.toString())
+    }
 
-  useEffect(() => {
-    const fechaActual = new Date();
-    const formateador = new Intl.DateTimeFormat('es-ES', { month: 'long' });
-    const mesActual = formateador.format(fechaActual);
-    const anioActual = fechaActual.getFullYear().toString()
-    
-    // filtra al cargar el componente
-    // setFilterMonth(mesActual)
-    // setFilterYear(anioActual)
+    const [nombreFecha, setFecha] = useState<{ mes: string | undefined; year: string | undefined }>({
+        mes: undefined,
+        year: undefined,
+    })
 
-    const mesCapitalizado = mesActual.charAt(0).toUpperCase() + mesActual.slice(1).toLowerCase();
-    
-    // setea mes y año actual para insertarlos en formularios
-    // si no existe mes o year no debería renderizar el componente(carga cliente)
-    // setFecha({
-    //   mes: mesCapitalizado,
-    //   year: anioActual
-    // })
-  }, [])
-  
-  return (
-    <div className="flex flex-row gap-6 justify-center mb-3">
-      <Autocomplete
-        onSelectionChange={monthSelectionChange}
-        // defaultSelectedKey={nombreFecha.mes}
-        onClick={(e) => e.stopPropagation()}
-        label="Filtrar por mes" className="max-w-xs" color="primary">
-        {months.map(month => (
-          <AutocompleteItem key={month} value={month}>{month}</AutocompleteItem>
-        ))}
-      </Autocomplete>
-      <Autocomplete
-        onSelectionChange={yearSelectionChange}
-        // defaultSelectedKey={nombreFecha.year}
-        onClick={(e) => e.stopPropagation()}
-        label="Filtrar por año" className="max-w-xs" color="primary">
-        {years.map(year => (
-          <AutocompleteItem key={year} value={year.toString()}>{year.toString()}</AutocompleteItem>
-        ))}
-      </Autocomplete>
-    </div>
-  );
-};
+    useEffect(() => {
+        const fechaActual = new Date()
+        const formateador = new Intl.DateTimeFormat('es-ES', { month: 'long' })
+        const mesActual = formateador.format(fechaActual)
+        const anioActual = fechaActual.getFullYear().toString()
+
+        // filtra al cargar el componente
+        // setFilterMonth(mesActual)
+        // setFilterYear(anioActual)
+
+        const mesCapitalizado = mesActual.charAt(0).toUpperCase() + mesActual.slice(1).toLowerCase()
+
+        // setea mes y año actual para insertarlos en formularios
+        // si no existe mes o year no debería renderizar el componente(carga cliente)
+        // setFecha({
+        //   mes: mesCapitalizado,
+        //   year: anioActual
+        // })
+    }, [])
+
+    return (
+        <div className="flex flex-row gap-6 justify-center mb-3">
+            <Autocomplete
+                onSelectionChange={monthSelectionChange}
+                // defaultSelectedKey={nombreFecha.mes}
+                onClick={(e) => e.stopPropagation()}
+                label="Filtrar por mes"
+                className="max-w-xs"
+                color="primary"
+            >
+                {months.map((month) => (
+                    <AutocompleteItem key={month} value={month}>
+                        {month}
+                    </AutocompleteItem>
+                ))}
+            </Autocomplete>
+            <Autocomplete
+                onSelectionChange={yearSelectionChange}
+                // defaultSelectedKey={nombreFecha.year}
+                onClick={(e) => e.stopPropagation()}
+                label="Filtrar por año"
+                className="max-w-xs"
+                color="primary"
+            >
+                {years.map((year) => (
+                    <AutocompleteItem key={year} value={year.toString()}>
+                        {year.toString()}
+                    </AutocompleteItem>
+                ))}
+            </Autocomplete>
+        </div>
+    )
+}
 
 export default FiltroVentas
--- src/components/Loginform.tsx
+++ src/components/Loginform.tsx
@@ -5,117 +5,114 @@ import { decodeJWT, isTokenExpired } from '@/utils/jwt'
 import storeAuth from '@/stores/store.auth'
 import { url } from '@/config/constants'
 import Image from 'next/image'
+import { Button } from '@nextui-org/react'
 
 // tipo para el estado del formulario
 type FormState = {
-  email: string
-  password: string
+    email: string
+    password: string
 }
 
 export default function LoginForm() {
-  const {isLogged, setIsLogged, user, isLoadingUser} = storeAuth()
-  const route = useRouter()
-  const [form, setForm] = useState<FormState>({
-    email: '',
-    password: '',
-  })
-  const [error, setError] = useState<string | null>(null)
-  const [message, setMessage] = useState<string | null>(null)
+    const { setIsLogged } = storeAuth()
+    const [loading, setLoading] = useState(false)
+    const route = useRouter()
+    const [form, setForm] = useState<FormState>({
+        email: '',
+        password: '',
+    })
+    const [error, setError] = useState<string | null>(null)
+    const [message, setMessage] = useState<string | null>(null)
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    let { name, value } = e.target
-    if(name === 'email') value = value.toLowerCase()
-    setForm({ ...form, [name]: value })
-  }
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault()
-    loginUser()
-  }
-
-  const loginUser = async () => {
-    try {
-      const response = await fetch(`${url.backend}/auth/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(form)
-      })
-      const data = await response.json()
-      if (response.ok) {
-        const resToken = response.headers.get('authorization')
-        if(resToken){
-          if(error) setError(null)
-          setMessage('Acceso correcto, redirigiendo..')
-          const [_bearer, token] = resToken.split(' ')
-          const decodeToken: any = decodeJWT(token)
-          if(decodeToken && isTokenExpired(decodeToken.exp)) throw 'El token expiró'
-          localStorage.setItem('user', JSON.stringify(decodeToken ))
-          localStorage.setItem('token', token) // Guardar el token en localStorage
-          setIsLogged(true) 
-          route.push('/')
-        }
-      } else {
-        const {error} = data
-        if(typeof error === 'string') setError(error)
-        if(typeof error === 'object') setError(error.message)
-        return console.error('Rechazado en auth', error)
-      }
-    } catch (error) {
-      return console.error('Error en la solicitud:', error)
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        let { name, value } = e.target
+        if (name === 'email') value = value.toLowerCase()
+        setForm({ ...form, [name]: value })
     }
-  }
-  // useEffect(() => {
-  //   if(!user){
-  //     route.refresh()
-  //   }
-  // }, [])
-  return (
-    <div className='bg-blue-900 dark:bg-gray-900 py-10 mt-20'>
-    <section className='shadow-md px-10'>
-      <Image src='/media/two-brands.png' alt="logo" width={200} height={100} className='mx-auto'/>
-      {error && <p className='bg-red-700 text-sm italic font-light text-white px-2 text-center rounded-lg py-1'>{error}</p>}
-      {message && <p className='bg-green-700 text-sm italic font-light text-white px-2 text-center rounded-lg py-1'>{message}</p>}
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-sm font-medium text-gray-200 dark:text-gray-400 mb-2">
-            Usuario:
-          </label>
-          <input
-            type="text"
-            autoComplete='off'
-            id="email"
-            name="email"
-            value={form.email}
-            onChange={handleChange}
-            className="w-full p-2 border rounded-md dark:bg-gray-700 dark:text-gray-300"
-            />
-        </div>
 
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-sm font-medium text-gray-200 dark:text-gray-400 mb-2">
-            Contraseña:
-          </label>
-          <input
-            type="password"
-            autoComplete='off'
-            id="password"
-            name="password"
-            value={form.password}
-            onChange={handleChange}
-            className="w-full p-2 border rounded-md dark:bg-gray-700 dark:text-gray-300"
-            />
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        loginUser()
+    }
+
+    const loginUser = async () => {
+        try {
+            setLoading(true)
+            const response = await fetch(`${url.backend}/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(form),
+            })
+            const data = await response.json()
+            if (response.ok) {
+                const resToken = response.headers.get('authorization')
+                if (resToken) {
+                    if (error) setError(null)
+                    setMessage('Acceso correcto, redirigiendo..')
+                    const [_bearer, token] = resToken.split(' ')
+                    const decodeToken: any = decodeJWT(token)
+                    if (decodeToken && isTokenExpired(decodeToken.exp)) throw 'El token expiró'
+                    localStorage.setItem('user', JSON.stringify(decodeToken))
+                    localStorage.setItem('token', token) // Guardar el token en localStorage
+                    setIsLogged(true)
+                    route.push('/')
+                }
+            } else {
+                const { error } = data
+                if (typeof error === 'string') setError(error)
+                if (typeof error === 'object') setError(error.message)
+                return console.error('Rechazado en auth', error)
+            }
+        } catch (error) {
+            return console.error('Error en la solicitud:', error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className="bg-blue-900 dark:bg-gray-900 py-10 mt-20">
+            <section className="shadow-md px-10">
+                <Image src="/media/two-brands.png" alt="logo" width={200} height={100} className="mx-auto" />
+                {error && <p className="bg-red-700 text-sm italic font-light text-white px-2 text-center rounded-lg py-1">{error}</p>}
+                {message && <p className="bg-green-700 text-sm italic font-light text-white px-2 text-center rounded-lg py-1">{message}</p>}
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-200 dark:text-gray-400 mb-2">
+                            Usuario:
+                        </label>
+                        <input
+                            type="text"
+                            autoComplete="off"
+                            id="email"
+                            name="email"
+                            value={form.email}
+                            onChange={handleChange}
+                            className="w-full p-2 border rounded-md dark:bg-gray-700 dark:text-gray-300"
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-200 dark:text-gray-400 mb-2">
+                            Contraseña:
+                        </label>
+                        <input
+                            type="password"
+                            autoComplete="off"
+                            id="password"
+                            name="password"
+                            value={form.password}
+                            onChange={handleChange}
+                            className="w-full p-2 border rounded-md dark:bg-gray-700 dark:text-gray-300"
+                        />
+                    </div>
+                    <Button type="submit" isLoading={loading} disabled={loading} color="primary">
+                        Entrar
+                    </Button>
+                </form>
+            </section>
         </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white p-2 rounded-md hover-bg-blue-600 focus:outline-none focus:bg-blue-600"
-          >
-          Entrar
-        </button>
-      </form>
-    </section>
-  </div>
-  )
+    )
 }
-
