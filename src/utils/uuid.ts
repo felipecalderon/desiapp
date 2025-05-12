111634076a9bc808daf4b01951d336185f5a8e5f
Felipe Eduardo Calderon Espinoza <felipe.calderon321@gmail.com>
2025-05-12 14:31:26 -0400

    excel barcode generator

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,6 +1,5 @@
 export const generateUUID = () =>
-    '1xxx-yxxx-xxxx-xxxx'.replace(/[xy]/g, (c) => {
-        const r = (Math.random() * 16) | 0,
-            v = c === 'x' ? r : (r & 0x3) | 0x8
-        return v.toString(16)
+    '1xxxxxxxxxxxx'.replace(/[x]/g, () => {
+        const r = Math.floor(Math.random() * 10)
+        return r.toString()
     })
